5e37ecb86975e7c5ce92cc989ffddd16b1def1a0
Acheampongjoojo <acheampongjoojo29@gmail.com>
2025-07-24 02:56:36 -0700

    first commit

---
+++ src/components/Accordian/data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        id : '1',
+        question: "What are accordian components?",
+        answer: "Accordian components are user interface elements used for organizing and presenting content in a collapsible manner."
+    },
+    {
+        id: "2",
+        question: "What are they used for?",
+        answer: "They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels and data tables to save screen space and provide a structured and user-friendly interface for presenting information or options"
+    },
+    {
+        id: "3",
+        question: "Accordian as a musical instrument",
+        answer: "The accordian is a musical instrument with a keyboard and bellows. It produces sound by air passing over reeds when the player expands or compresses the bellows, used in various music generes"
+    },
+    {
+        id: "4",
+        question: "Can I create an accordian component with a different framework",
+        answer: "Yes of course, it is very possible to create an accordian component with another framework"
+    },
+    
+];
+
+export default data;
---
+++ src/components/Accordian/index.jsx
@@ -0,0 +1,41 @@
+//single selection
+//multiple selection
+
+import { useState } from "react";
+import data from "./data";
+import "./styles.css"
+
+export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
+
+    console.log(selected);
+
+
+    return (
+        <div className="wrapper">
+            <button className="header">Single Selection Accordion</button>
+            <div className="accordian">
+                {
+                    data && data.length > 0 ?
+                        data.map(dataItem => <div className="item">
+                            <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {
+                                selected === dataItem.id ?
+                                    <div className="content">{dataItem.answer}</div>
+                                    : null
+                            }
+                        </div>
+                        )
+                        : (<div>No data found</div>)
+                }
+            </div>
+
+        </div>
+    );
+}
