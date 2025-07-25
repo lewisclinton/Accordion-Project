7820e8e47b28d9fcf1659b82e7fd23330f20ada4
Acheampongjoojo <acheampongjoojo29@gmail.com>
2025-07-25 02:05:24 -0700

    first commit

--- src/components/Accordian/data.js
+++ src/components/Accordian/data.js
@@ -7,17 +7,17 @@ const data = [
     {
         id: "2",
         question: "What are they used for?",
-        answer: "They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels and data tables to save screen space and provide a structured and user-friendly interface for presenting information or options"
+        answer: "They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels and data tables to save screen space and provide a structured and user-friendly interface for presenting information or options."
     },
     {
         id: "3",
         question: "Accordion as a musical instrument",
-        answer: "The accordion is a musical instrument with a keyboard and bellows. It produces sound by air passing over reeds when the player expands or compresses the bellows, used in various music generes"
+        answer: "The accordion is a musical instrument with a keyboard and bellows. It produces sound by air passing over reeds when the player expands or compresses the bellows, used in various music generes."
     },
     {
         id: "4",
         question: "Can I create an accordion component with a different framework",
-        answer: "Yes of course, it is very possible to create an accordian component with another framework"
+        answer: "Yes of course, it is very possible to create an accordian component with another framework."
     },
     
 ];
